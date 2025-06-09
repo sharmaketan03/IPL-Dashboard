9e4b153c5bb9ff63c4fcc87305c8f1696e1f38d4
sharmaketan03 <ketan301024@gmail.com>
2025-06-09 14:29:13 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import Header from './header.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <Header />
-  </StrictMode>,
+
 )
